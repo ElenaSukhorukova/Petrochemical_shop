7c16588ff65e6bcc972b9a8839b609aeed3cc67f
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-20 16:27:10 +0400

    Add and configure devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :users
   get 'static_pages/about'
   get 'static_pages/delivery'
   get 'static_pages/contacts'
+
+  root to: "products#index"
 end
