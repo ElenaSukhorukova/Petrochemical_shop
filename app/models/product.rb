49674d12215fdf638d3c2c34b14002ea32f5d26d
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-23 10:51:51 +0400

    Update associations in product's model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,15 @@
 class Product < ApplicationRecord
-  has_many :department_products
-  has_many :departments, through: :department_products
+  has_many :department_products, dependent: :destroy
+  has_many :departments, through: :department_products, dependent: :destroy
+
+  belongs_to :kind
+
+  has_many :line_items, dependent: :destroy
+
+  has_many :container_products, dependent: :destroy
+  has_many :containers, through: :container_products, dependent: :destroy
+
+  has_many :entrances, as: :entrancable_item, plymorphic: true, dependent: :destroy
+  
+  has_many :order_packagings, dependent: :destroy
 end
