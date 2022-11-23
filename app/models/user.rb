1440210b2a22f97741d641463fdf7eab6d2686e8
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-11-23 10:52:42 +0400

    Add associations in user's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :company_residents, dependent: :destroy
+  has_many :company_non_residents, dependent: :destroy
 end
