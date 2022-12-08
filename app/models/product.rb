77e95fa87f1eb0082b726db154a9ed042a6c8750
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-08 15:17:29 +0400

    Update product's model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,15 +1,8 @@
 class Product < ApplicationRecord
-  has_many :department_products, dependent: :destroy
-  has_many :departments, through: :department_products, dependent: :destroy
+  validates :name, presence: true, length: { minimum: 3 }
 
   belongs_to :kind
-
-  has_many :line_items, dependent: :destroy
-
-  has_many :container_products, dependent: :destroy
-  has_many :containers, through: :container_products, dependent: :destroy
-
-  has_many :entrances, as: :entrancable_item, plymorphic: true, dependent: :destroy
   
-  has_many :order_packagings, dependent: :destroy
+  has_many :department_products
+  has_many :departments, through: :department_products
 end
