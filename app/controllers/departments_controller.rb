b7d99e3b6705258781304559f83bb9ee9db8a388
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-08 15:00:08 +0400

    Add deparments' controller

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,2 +1,18 @@
 class DepartmentsController < ApplicationController
+  before_action :authenticate_user!
+  
+  def index
+    @departments = Department.all
+  end
+
+  def show
+    @department = Department.find(params[:id])
+    @products = Product.all
+  end
+
+  private
+
+    def department_params
+      params.require(:department).permit(:name_department, :description)
+    end
 end
