fcc14a0d21a2f609425a316eda91a1b9205caaf9
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-08 15:16:24 +0400

    Add company_contact's model

--- app/models/company_contact.rb
+++ app/models/company_contact.rb
@@ -1,3 +1,12 @@
 class CompanyContact < ApplicationRecord
-  belongs_to :contactabable, polymorphic: true
+  validates :full_name, :position, presence: true, length: {minimum: 3 }
+  validates :additional_phone_number, presence: true, length: { is: 5 }
+  validates :personal_phone_number, :work_phone_number, presence: true, 
+    format: { 
+      with: /(\d|\+\d)+\d{10}/,
+      message: 'A phone number should be 11 numbers'
+    }
+  validates :email, presence: true, format: { with: /([\w\-\.])+@([\w\-\.]+\.)+([a-z]{2,4})/ }
+
+  belongs_to :company
 end
