9186bf6d64ee83efbea8b4c255aebbcd971575ea
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-08 15:08:27 +0400

    Add concerns

---
+++ app/controllers/concerns/change_full_name.rb
@@ -0,0 +1,13 @@
+module ChangeFullName
+  extend ActiveSupport::Concern
+
+  def change_full_name(model)
+    if %w[Supplier Client OurCompany].include?(model.class)
+      director = model.full_name_director.split(' ').map(&:capitalize).join(' ')
+      model.update(full_name_director: director)
+    else
+      contact = model.full_name.split(' ').map(&:capitalize).join(' ')
+      model.update(full_name: contact)
+    end
+  end
+end
