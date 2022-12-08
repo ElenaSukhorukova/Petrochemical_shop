5fa0c76a0fe0e611400ebb155dd2a097321b5fab
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-08 15:25:02 +0400

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,44 @@
 Rails.application.routes.draw do
-  devise_for :admins
   devise_for :users
-  get 'static_pages/about'
-  get 'static_pages/delivery'
-  get 'static_pages/contacts'
+  
+  devise_scope :user do  
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+    get 'sign_out', to: 'devise/sessions#destroy'
+  end
 
-  root to: "products#index"
+  get 'about', to: 'static_pages#about'
+  get 'delivery', to: 'static_pages#delivery'
+  get 'contacts', to: 'static_pages#contacts'
+
+  root to: 'static_pages#about'
+
+  scope module: :user do
+    resources :admins
+    resources :supply_managers
+  end
+
+  scope module: :admins, as: 'admin' do
+    resources :departments, except: %i[index show]
+  end
+
+  scope module: :supply_managers, as: 'supply_managers' do
+    resources :kinds
+    resources :products
+  end
+
+  scope module: :supply_managers do
+    resources :suppliers
+  end
+
+  scope module: :companies do
+    resources :companies, only: %i[show] do
+      resources :company_contacts, only: %i[new create edit update destroy], shallow: true
+    end
+  end
+  
+  resources :departments, only: %i[index show]
+  resources :products, only: %i[index show]
+  resources :banks
+
 end
