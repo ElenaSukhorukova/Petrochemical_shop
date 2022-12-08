4037fd8276507e5ecd807ef489e9f4bf00db4a42
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-08 14:59:21 +0400

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  include ErrorHandling
+  include ApplicationHelper
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  add_flash_types :info, :danger, :warning, :success, :alert, :notice
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
+    end
 end
