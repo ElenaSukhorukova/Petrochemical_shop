394af78dcbc71c065b482d3e43dcf93238f4317c
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-11 02:11:06 +0400

    Edit products' model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
-  validates :name, presence: true, length: { minimum: 3 }
+  validates :name, presence: true, uniqueness: true, length: { minimum: 3 }
 
   belongs_to :kind
-  
   has_many :department_products
   has_many :departments, through: :department_products
+  has_many :line_items, as: :product, dependent: :destroy
 end
