c3258ad55dd22edeeac4cdcca32795b1b306b7a6
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-11 02:18:41 +0400

    Add new routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,31 +14,51 @@ Rails.application.routes.draw do
   root to: 'static_pages#about'
 
   scope module: :user do
-    resources :admins
-    resources :supply_managers
+    resources :admins, except: %i[index]
+    resources :supply_managers, except: %i[index]
+    resources :cheifs, except: %i[index]
   end
 
   scope module: :admins, as: 'admin' do
     resources :departments, except: %i[index show]
   end
 
+  scope module: :cheifs do
+    resources :our_companies
+  end
+
   scope module: :supply_managers, as: 'supply_managers' do
-    resources :kinds
     resources :products
   end
-
+  
   scope module: :supply_managers do
+    resources :kinds, only: %i[new create edit update]
     resources :suppliers
+    resources :receptions
+    resources :containers
+  end  
+
+  resources :receptions, only: %i[show] do      
+    scope module: :receptions, as: 'reception' do 
+      resources :line_items, shallow: true
+      resources :line_item_containers, shallow: true
+    end
   end
 
-  scope module: :companies do
-    resources :companies, only: %i[show] do
+  scope module: :suppliers do
+    resources :suppliers, only: %i[show] do
       resources :company_contacts, only: %i[new create edit update destroy], shallow: true
     end
   end
-  
+
+  scope module: :our_companies do
+    resources :our_companies, only: %i[show] do
+      resources :company_contacts, only: %i[new create edit update destroy], shallow: true
+    end
+  end
+
   resources :departments, only: %i[index show]
   resources :products, only: %i[index show]
   resources :banks
-
+  resources :kinds, only: %i[index]
 end
