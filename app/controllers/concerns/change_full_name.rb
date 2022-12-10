07e2b3a849ce066e678a4eb48bf02383093f6f17
Elena Sukhorukova <suhorukova.el@gmail.com>
2022-12-11 02:02:59 +0400

    Add method to edit full name

--- app/controllers/concerns/change_full_name.rb
+++ app/controllers/concerns/change_full_name.rb
@@ -2,7 +2,7 @@ module ChangeFullName
   extend ActiveSupport::Concern
 
   def change_full_name(model)
-    if %w[Supplier Client OurCompany].include?(model.class)
+    if %w[Supplier Client OurCompany].include?(model.class.to_s)
       director = model.full_name_director.split(' ').map(&:capitalize).join(' ')
       model.update(full_name_director: director)
     else
